53d7a0c0aeba1cf9a809654aeea92d4e6a9e5981
Ricardo Deodutt <RicardoDeodutt@gmail.com>
2021-02-16 23:19:06 -0500

    Version 1 of Moron Test

---
+++ 2020/Web Development 1/moron-test/v1/assets/js/moron.js
@@ -0,0 +1,44 @@
+const yellow = '#f9d71c';
+const grey = '#c2bfb8';
+const blue = '#3babfd';
+const lightBlue = '#abf0ff';
+const transparent = '#ffffff00';
+
+
+let scene25Icons = [{
+        class: ["fas", "fa-cloud-showers-heavy"],
+        color: blue,
+        content: null,
+        size: 14,
+        position: [50, 25],
+        transform: null,
+        filter: null,
+    },
+    {
+        class: ["fas", "fa-sun"],
+        color: yellow,
+        content: null,
+        size: 14,
+        position: [25, 50],
+        transform: null,
+        filter: null,
+    },
+    {
+        class: ["fas", "fa-cloud"],
+        color: grey,
+        content: null,
+        size: 14,
+        position: [75, 50],
+        transform: null,
+        filter: null,
+    },
+    {
+        class: ["fas", "fa-snowflake"],
+        color: lightBlue,
+        content: null,
+        size: 14,
+        position: [50, 75],
+        transform: null,
+        filter: null,
+    }
+]
